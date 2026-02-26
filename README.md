7bd355adab6f885e6d16e605f7ec47ab26ccba7a
Richard Hildred <rhildred@gmail.com>
2026-02-26 11:47:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# preact-native-custom-element
+## with react-native-paper, tailwind and uniwind
+
+```jsx
+    <PaperProvider>
+      <View className="p-[10px]">
+        <Text variant="headlineMedium">Welcome to React Native Paper with Vite!</Text>
+        <Button mode="contained" onPress={() => console.log('Pressed')} className="mt-[15px]">
+          Press me
+        </Button>
+      </View>
+    </PaperProvider>
+```
+
+1. I need html5 custom elements/web components that can be also be used in a react-native app on a phone. 
+2. I want to be able to style these components with tailwind.
+3. I need a richer array of components than the react-native core components.
+
+I was encouraged by [Daniel Williams work](https://github.com/dannyhw/react-native-web-vite-sb-examples) that it is indeed possible for me to have what I need. Thanks to Daniel Williams help with uniwind as the glue for this I now have it working. I hope that others, including my students at Wilfrid Laurier university, will be able to also use this to make components that can be used in progressive web apps or in react native.
---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import { rnw } from "vite-plugin-rnw";
+import { uniwind } from "uniwind/vite";
+import path from "path";
+
+const rnwLayeredStyleSheet = {
+  enforce: "pre",
+  name: "rnw-layered-stylesheet",
+  resolveId(source, importer) {
+    const isTarget =
+      source === "./createOrderedCSSStyleSheet" ||
+      source.endsWith(
+        "react-native-web/dist/exports/StyleSheet/dom/createOrderedCSSStyleSheet.js",
+      );
+    if (
+      isTarget &&
+      importer?.includes("react-native-web/dist/exports/StyleSheet")
+    ) {
+      return path.resolve(
+        "node_modules/uniwind/dist/module/components/web/createOrderedCSSStyleSheet.js",
+      );
+    }
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // @ts-ignore
+    rnwLayeredStyleSheet,
+    rnw(),
+    preact(),
+    tailwindcss(),
+    uniwind({
+      // relative path to your global.css file (from previous step)
+      cssEntryFile: "./src/index.css",
+      // (optional) path where we gonna auto-generate typings
+      // defaults to project's root
+      dtsFile: "./src/uniwind-types.d.ts",
+    }),
+  ],
+  server: { allowedHosts: true },
+    build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "x-works.js",
+        assetFileNames: "x-works.[ext]",
+      },
+    }
+  }
+});
